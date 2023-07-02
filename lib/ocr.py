3b1d6b5cdadaa58937b6a4e46bba9cb90bacd265
Martín Lombardo <to@mlomb.me>
2023-07-02 14:50:44 -0300

    better OCR

--- lib/ocr.py
+++ lib/ocr.py
@@ -2,8 +2,11 @@ from dataclasses import dataclass
 from PIL import Image
 
 import pytesseract
+import easyocr
 
 from lib.text import try_extract_from_text
+
+reader = easyocr.Reader(["en"], gpu=True)
 
 
 @dataclass
@@ -27,7 +30,8 @@ def ocr_images(image_paths: list[str], params: ImagePreprocessParams):
     # combine texts from all images
     text = ""
     for image_path in image_paths:
-        text += pytesseract.image_to_string(
-            preprocess_image(Image.open(image_path), params)
-        )
+        text += "\n".join(reader.readtext(image_path, detail=0, batch_size=50))  # type: ignore
+        # text += pytesseract.image_to_string(
+        #    preprocess_image(Image.open(image_path), params)
+        # )
     return text
